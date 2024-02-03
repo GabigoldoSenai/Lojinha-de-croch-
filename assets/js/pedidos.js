d3a54f8146096d29e4f8d0ea55d355d31a876dde
Letícia G. Soares <leticiag.soares.dev@gmail.com>
2024-02-03 19:05:46 -0300

    opened card issue fixed

--- assets/js/pedidos.js
+++ assets/js/pedidos.js
@@ -22,58 +22,63 @@ let escolha = prompt("[A]: Catálogo\n [B]: Encomenda")
 
 const requestCrochetBtn = document.querySelector("requestCrochet")
 const modal = document.querySelector("dialog")
+let isPopupOpened = false
 
 function knowMore(imgURL, nameCrochet, categCrochet, descCrochet, priceCrochet) {
+    if (isPopupOpened) {
+        closePopup()
+    }
+    isPopupOpened = true
     const popupContainer = document.createElement('div')
     popupContainer.classList.add("requestPopup")
 
     const newHTML = `
-        <button class="closePopupBtn" onclick="closePopup()">&times</button>
-        <img src="${imgURL}" class="imgPopup" alt="${nameCrochet}">
-        <div class="textPopup">
-            <p class="categoryCrochet">${categCrochet}</p>
-            <h2 class="nameCrochet">${nameCrochet}</h2>
-            <h3 class="subtitlePopup">Descrição:</h3>
-            <p>${descCrochet}</p>
-            <h3 class="subtitlePopup">Valor:</h3>
-            <h2 class="priceCrochet">R$${priceCrochet.toFixed(2)}</h2>
+            <button class="closePopupBtn" onclick="closePopup()">&times</button>
+            <img src="${imgURL}" class="imgPopup" alt="${nameCrochet}">
+            <div class="textPopup">
+                <p class="categoryCrochet">${categCrochet}</p>
+                <h2 class="nameCrochet">${nameCrochet}</h2>
+                <h3 class="subtitlePopup">Descrição:</h3>
+                <p>${descCrochet}</p>
+                <h3 class="subtitlePopup">Valor:</h3>
+                <h2 class="priceCrochet">R$${priceCrochet.toFixed(2)}</h2>
 
-            <h3 class="subtitlePopup">Preencha os dados</h3>
-            <form class="requestForm">
-                <input type="text" class="getNameClient" required placeholder="Seu Nome/Apelido">
-                <input type="number" class="getQntCrochet" required placeholder="Quantidade">
+                <h3 class="subtitlePopup">Preencha os dados</h3>
+                <form class="requestForm">
+                    <input type="text" class="getNameClient" required placeholder="Seu Nome/Apelido">
+                    <input type="number" class="getQntCrochet" required placeholder="Quantidade">
 
-                <div class="selectBox">
-                    <select id="getTypeDelivery" required>
-                        <option selected value="null">Entrega</option>
-                        <option value="Delivery">Delivery</option>
-                        <option value="Presencial">Presencial</option>
-                    </select>
+                    <div class="selectBox">
+                        <select id="getTypeDelivery" required>
+                            <option selected value="null">Entrega</option>
+                            <option value="Delivery">Delivery</option>
+                            <option value="Presencial">Presencial</option>
+                        </select>
 
-                    <select id="getTypePayment" name="getTypePayment" required>
-                        <option selected value="null">Pagamento</option>
-                        <option value="PIX">PIX</option>
-                        <option value="Dinheiro">Dinheiro</option>
-                    </select>
-                </div>
+                        <select id="getTypePayment" name="getTypePayment" required>
+                            <option selected value="null">Pagamento</option>
+                            <option value="PIX">PIX</option>
+                            <option value="Dinheiro">Dinheiro</option>
+                        </select>
+                    </div>
 
-                <button type="button" class="requestCrochet">Criar Pedido</button>
-            </form>
-        </div>
-    `
+                    <button type="button" class="requestCrochet">Criar Pedido</button>
+                </form>
+            </div>
+        `
 
     popupContainer.innerHTML = newHTML
 
     popupContainer.style.transition = "transform 0.3s, opacity 0.3s"
-    popupContainer.style.opacity = "0" 
-    popupContainer.style.transform = "translateX(360px)" 
+    popupContainer.style.opacity = "0"
+    popupContainer.style.transform = "translateX(360px)"
 
     setTimeout(() => {
         popupContainer.style.opacity = "1"
         popupContainer.style.transform = "translateX(0)"
     }, 10)
 
-    popupContainer.addEventListener("click", function(event) {
+    popupContainer.addEventListener("click", function (event) {
         if (event.target.classList.contains("requestCrochet")) {
             const nameClient = document.querySelector(".getNameClient").value
             const qntCrochet = document.querySelector(".getQntCrochet").value
@@ -81,16 +86,18 @@ function knowMore(imgURL, nameCrochet, categCrochet, descCrochet, priceCrochet) 
             const typePayment = document.querySelector("#getTypePayment").value
 
 
-                if (!nameClient || nameClient === "null" || !qntCrochet || qntCrochet === "null" ||
-            !typeDelivery || typeDelivery === "null" || !typePayment || typePayment === "null") {
-            alert("Por favor, preencha todos os campos corretamente antes de enviar o pedido.")
-            return 
-        }
+            if (!nameClient || nameClient === "null" || !qntCrochet || qntCrochet === "null" ||
+                !typeDelivery || typeDelivery === "null" || !typePayment || typePayment === "null") {
+                alert("Por favor, preencha todos os campos corretamente antes de enviar o pedido.")
+                return
+            }
             requestCrochet(nameCrochet, categCrochet, priceCrochet, nameClient, qntCrochet, typePayment, typeDelivery)
         }
     })
 
     document.body.appendChild(popupContainer)
+
+
 
 }
 
@@ -114,24 +121,25 @@ function requestCrochet(nameCrochet, categCrochet, priceCrochet, nameClient, qnt
         </div>`
 
 
-    document.body.appendChild(finishPopup)    
+    document.body.appendChild(finishPopup)
     finishPopup.innerHTML = newHTML
 
 }
 
 function closePopup() {
+    isPopupOpened = false
     const requestCrochetPopup = document.querySelector(".requestPopup")
 
     requestCrochetPopup.style.transition = "transform 0.3s, opacity 0.3s"
-    requestCrochetPopup.style.opacity = "0" 
-    requestCrochetPopup.style.transform = "translateX(100%)" 
+    requestCrochetPopup.style.opacity = "0"
+    requestCrochetPopup.style.transform = "translateX(100%)"
 
     requestCrochetPopup.addEventListener("transitionend", function () {
         requestCrochetPopup.remove()
     })
 }
 
-function closeFinishPopup(){
+function closeFinishPopup() {
     const finishPopup = document.querySelector(".finishPopup")
     finishPopup.remove()
 }
