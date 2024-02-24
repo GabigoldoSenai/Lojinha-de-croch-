bdc1548d2ebcde6db0548994801d7301c5c64f79
Letícia G. Soares <138785533+GabigoldoSenai@users.noreply.github.com>
2024-02-23 22:06:59 -0300

    Update commands.js

--- assets/js/commands.js
+++ assets/js/commands.js
@@ -9,15 +9,15 @@ criarCardFirstCatalog(1, "https://preview.redd.it/7q51bdrg4ogc1.png?width=500&fo
 criarCardFirstCatalog(1, "https://preview.redd.it/y8yo8arg4ogc1.png?width=500&format=png&auto=webp&s=7192510e4476ed5502fe577db5db3f3c40e5289a", 1, "Tamanho: 7cm x 7,5cm", "Cogumelo", 15)
 criarCardFirstCatalog(1, "https://preview.redd.it/7hca6crg4ogc1.png?width=500&format=png&auto=webp&s=923a3f7d0f5c1c007614a1ce793a9d0daa8d4f54", 1, "Tamanho: 7,5cm x 5,5cm", "Pato", 15)
 criarCardFirstCatalog(1, "https://preview.redd.it/lwussbrg4ogc1.png?width=500&format=png&auto=webp&s=dc4403ac1fa168ff454c602df8a8132478f6bbdc", 0, "Tamanho: 3,5cm x 4,5cm", "Abelha", 8)
-criarCardFirstCatalog(1, "https://preview.redd.it/78in3drg4ogc1.png?width=500&format=png&auto=webp&s=a19d21c9267a1cc5f640c1e234f0cac8e41c62f8", 1, "Tamanho: 8cm x 5,5cm <br> Polvo reversível com duas carinhas", "Polvo", 8)
-criarCardFirstCatalog(1, "https://preview.redd.it/otz52brg4ogc1.png?width=500&format=png&auto=webp&s=0f9a65c2747a1d8e6ed9b57a594ddfe966c2d608", 0, "Tamanho: 4,5cm x 3,5cm <br> Polvo reversível com duas carinhas", "Pikachu", 5)
+criarCardFirstCatalog(2, "https://preview.redd.it/78in3drg4ogc1.png?width=500&format=png&auto=webp&s=a19d21c9267a1cc5f640c1e234f0cac8e41c62f8", 1, "Tamanho: 8cm x 5,5cm <br> Polvo reversível com duas carinhas", "Polvo", 15)
+criarCardFirstCatalog(1, "https://preview.redd.it/otz52brg4ogc1.png?width=500&format=png&auto=webp&s=0f9a65c2747a1d8e6ed9b57a594ddfe966c2d608", 0, "Tamanho: 4,5cm x 3,5cm", "Pikachu", 5)
 criarCardFirstCatalog(1, "https://preview.redd.it/287ajfrg4ogc1.png?width=500&format=png&auto=webp&s=1b86fba535c2f9d4b342ec6aeb6cee6009e13afe", 0, "Tamanho: 8cm x 3,5cm", "Quadribol", 15)
 criarCardFirstCatalog(1, "https://preview.redd.it/klm8lerg4ogc1.png?width=500&format=png&auto=webp&s=132324fb22c6dfa3081a0d107e73beb6ebe63a39", 0, "Tamanho: 3,5cm x 3,5cm", "Kirby", 8)
 
 
 
 criarCardFirstCatalog(2, "https://preview.redd.it/v0r68arg4ogc1.png?width=500&format=png&auto=webp&s=949e6b4056e87578d0005db6400d5e2630fa7ae5", 2, "Altura: 8cm", "Miles Morales", 30)
-criarCardFirstCatalog(2, "https://preview.redd.it/rcnr2w2d4ogc1.png?width=500&format=png&auto=webp&s=041e35b4b995091451145f1384574d2536a3ffeb", 2, "Altura: 10cm", "Totoro", 30)
+criarCardFirstCatalog(2, "https://preview.redd.it/rcnr2w2d4ogc1.png?width=500&format=png&auto=webp&s=041e35b4b995091451145f1384574d2536a3ffeb", 2, "Altura: 10cm", "Totoro", 25)
 criarCardFirstCatalog(2, "https://preview.redd.it/1de8emrj4ogc1.png?width=500&format=png&auto=webp&s=6f6d1b4da05ce59b13f239aaa14b011218a03136", 2, "Tamanho: 4,5cm x 3,5cm", "Pochita", 8)
 criarCardFirstCatalog(2, "https://preview.redd.it/fz9e8lqj4ogc1.png?width=500&format=png&auto=webp&s=0306e9d1445f69d09dce17fcea48b36bcfe54f15", 2, "Altura: 10cm", "My Melody", 25)
 criarCardFirstCatalog(2, "https://preview.redd.it/fs2udjqj4ogc1.png?width=500&format=png&auto=webp&s=3e62cb9c57c65359db2c02e2ea1723dba7bfb63f", 2, "Altura: 10cm", "Sonic", 30)
